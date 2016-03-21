204931ba116c9319f24ea9d70be825679f236a0c
yuhanluo <yuhan.luo111@gmail.com>
2016-03-21 15:59:51 -0400

    modify

--- simplereading/translate/utils.py
+++ simplereading/translate/utils.py
@@ -21,7 +21,7 @@ corpus += r
 fdist = nltk.FreqDist(w.lower() for w in corpus)
 
 
-def simplify(s):
+def simplify_old(s):
     res = ''
     st = LancasterStemmer()
 
@@ -55,3 +55,38 @@ def simplify(s):
 
         res = res + word + ' '
     return res
+
+def simplify(s, min_frequent=100, min_frequent_diff = 1.2):
+	res = ''
+	st = LancasterStemmer()
+
+	text = nltk.word_tokenize(s)
+	tags = nltk.pos_tag(text)
+
+	for tag in tags:
+		word = tag[0]
+		if f.checkPos(tag[1]):
+			if word in model:
+				if fdist[word] < min_frequent:  ## the min frequent less than 
+					word_stem = st.stem(word)  
+					top_words = model.most_similar(positive=[word], topn = 20)
+					candidate_list = [w[0] for w in top_words]
+					freq_list = [fdist[w] for w in candidate_list]
+					c_f_list = zip(candidate_list, freq_list)
+					ordered_list = sorted(c_f_list, key=lambda c_f_list:c_f_list[1], reverse=True)
+					word_freq = fdist[word]
+		#			synonmys = f.getSynonmys(word)  ## get synonmys from wordnet
+					# print synonmys
+					for w in ordered_list:
+						if word_freq * min_frequent_diff >= w[1]:  ## break for loop if candidate word frequency does not exceed the word frequency by a threshold 
+							break
+						if st.stem(w[0]) != word_stem and f.samePos(word, w[0]): ##exclude morphological derivations and same pos
+							word = w[0]  ### do not use wordnet
+							# if w[0] in synonmys:
+							# 	word = w[0]
+							# else:
+							# 	for syn in synonmys:
+							# 		if st.stem(w[0]) == st.stem(syn):
+							# 			word = w[0]
+		res = res + word + ' '	
+	return res
