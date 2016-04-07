e4c4579698e88f0f3178207cd5598444229a9c2e
Yuhan Luo <Hanna@Yuhans-MacBook-Pro.local>
2016-04-07 12:02:10 -0400

    test

--- simplereading/translate/forms.py
+++ simplereading/translate/forms.py
@@ -3,13 +3,13 @@ from .models import Simple, Original, Vote, Simplify, Comment
 from django.utils.safestring import mark_safe
 
 class OriginForm(forms.ModelForm):
-    hard_text = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}), label='')
+    hard_text = forms.CharField(widget=forms.Textarea(attrs={'rows':5, 'cols':60}), label='')
     class Meta:
         model = Original
         fields = ('hard_text',)
 
 class TestForm(forms.Form):
-     original_text = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}), label='')
+     original_text = forms.CharField(widget=forms.Textarea(attrs={'rows':5, 'cols':60}), label='')
 
 class SimpForm(forms.ModelForm):
     simple_text = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}), label='')
@@ -18,20 +18,20 @@ class SimpForm(forms.ModelForm):
         exclude = ('hard',)
 
 class CommentForm(forms.ModelForm):
-    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}), label=mark_safe('Leave a comment<br />'))
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':60}), label='')
     class Meta:
         model = Comment
         exclude = ('history',)
 
 class SimplifyForm(forms.ModelForm):
-    input = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}), label='')
-    output = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}), label='')
+    input = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}))
+    output = forms.CharField(widget=forms.Textarea(attrs={'rows':20, 'cols':80}))
     class Meta:
         model = Simplify
         fields = ('input','output',)
 
 class VoteForm(forms.ModelForm):
-    votes = forms.IntegerField()
+    votes = forms.IntegerField(required=True)
     class Meta:
         model = Vote
         fields = ('votes',)
